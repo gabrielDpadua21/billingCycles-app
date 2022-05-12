a4cc0bf0c609a05815b3d317a141d4ca963e9b38
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-05-11 22:27:32 -0300

    ✨feat(project): treat error messages

--- bc-api/src/api/billingCycle/billingCycle.js
+++ bc-api/src/api/billingCycle/billingCycle.js
@@ -3,12 +3,12 @@ const mongoose = restful.mongoose;
 
 const creditSchema = new mongoose.Schema({
     name: { type: String, required: true},
-    value: { type: Number, min: 0, required: true }
+    value: { type: Number, min: 0, required: [ true, 'Informe o valor crédito!' ] }
 });
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [ true, 'Informe o valor do débito!' ] },
     status: { type: String, required: false, uppercase: true, 
         enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 });
