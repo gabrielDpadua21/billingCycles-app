8daa6dfd283a65a2ec3e117770bcbcc47b817439
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-05-11 23:57:53 -0300

    ✨feat(project): create count billing route

--- bc-api/src/config/database.js
+++ bc-api/src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-module.exports = mongoose.connect('mongodb://bc_user:Toor1234@localhost/admin');
+module.exports = mongoose.connect('mongodb://bc_user:Toor1234@localhost/admin', 
+{ useNewUrlParser: true });
 
 mongoose.Error.messages.general.required = 'O atributo "{PATH}" é obrigatório';
 mongoose.Error.messages.Number.min = 
